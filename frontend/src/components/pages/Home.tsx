113ebc6ae060a62eb6f2487740bbab24a023bd72
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-14 14:30:48 +0100

    Implement hook

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,11 +1,36 @@
-import { FunctionComponent } from "react";
-import { Container } from "react-bootstrap";
+import { FunctionComponent, useCallback, useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://localhost:5000/",
+  responseType: "json",
+});
 
 export const Home: FunctionComponent = () => {
+  const loadLink = useCallback(async () => {
+    const response = await api.get("/link");
+    return response.data;
+  }, []);
+
+  const [link, setLink] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const link = await loadLink();
+    setLink(link);
+  }, [loadLink, setLink]);
+
   return (
     <Container>
-      <h2>Home</h2>
-      <p>Home</p>
+      <Row>
+        <Col>
+          <h2>Gift inspirator</h2>
+          <p>We've found a gift idea for you.</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
     </Container>
   );
 };
