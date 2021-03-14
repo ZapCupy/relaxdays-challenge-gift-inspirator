9aefe7bc6978f576085d8da71f6d1794fe2e4642
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-14 14:06:19 +0100

    Rewire backend

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,20 @@
 from flask import Flask, jsonify
+from backend.crawler import getRandomRelaxPage, getProductInfo
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 @app.route("/idea", methods=["GET"])
 def get_idea():
-    return jsonify("test")
+    idea = getRandomRelaxPage()
+    return jsonify(idea)
+
+
+@app.route("/info/<url>", methods=["GET"])
+def get_info(url):
+    info = getProductInfo(url)
+    return jsonify(info)
 
 
 app.run(host="0.0.0.0", debug=True)
